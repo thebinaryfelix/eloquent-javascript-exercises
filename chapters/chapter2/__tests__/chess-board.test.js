bf693143cccb53e2a41f33499601b60e24eaa900
Mateus Felix <mateus.cfoliveira@gmail.com>
2019-10-05 23:04:15 -0300

    test: create test files

---
+++ chapters/chapter2/__tests__/chess-board.test.js
@@ -0,0 +1,9 @@
+import { chessBoard } from '../chess-board'
+
+const expected8 = ''
+
+describe('chessBoard', () => {
+  test('draws a board of passed size', () => {
+    expect(chessBoard(8)).toBe()
+  })
+})
