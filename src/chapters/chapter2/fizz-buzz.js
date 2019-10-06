8254e532be23c22801e2db6efc798dc6938b55d3
Mateus Felix <mateus.cfoliveira@gmail.com>
2019-10-05 23:35:39 -0300

    refactor(fizz-buzz): remove console

--- src/chapters/chapter2/fizz-buzz.js
+++ src/chapters/chapter2/fizz-buzz.js
@@ -1,15 +1,16 @@
-const fizzBuzz = () => {
+export const fizzBuzz = () => {
+  let output = ''
   for (let i = 1; i <= 100; i += 1) {
     if (i % 3 === 0 && i % 5 === 0) {
-      console.log('FizzBuzz')
+      output += 'FizzBuzz\n'
     } else if (i % 3 === 0) {
-      console.log('Fizz')
+      output += 'Fizz\n'
     } else if (i % 5 === 0) {
-      console.log('Buzz')
+      output += 'Buzz\n'
     } else {
-      console.log(i)
+      output += `${i}\n`
     }
   }
-}
 
-fizzBuzz()
+  return output
+}
